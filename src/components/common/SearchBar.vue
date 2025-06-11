388eb8a88505ff6e1639f482e30f05fad8c8537e
VladanMilinkovic <37398679+VladanMilinkovic@users.noreply.github.com>
2025-06-11 23:58:07 +0300

    add clear button on search bar

--- src/components/common/SearchBar.vue
+++ src/components/common/SearchBar.vue
@@ -1,4 +1,3 @@
-<!-- src/components/SearchBar/SearchBar.vue -->
 <template>
   <div class="search-bar">
     <input
@@ -7,29 +6,22 @@
         placeholder="Search movies..."
         @input="handleInput"
     />
+    <button
+        class="clear-button"
+        :disabled="!searchInput.trim()"
+        @click="clearSearchHandler"
+    >
+      Clear
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from 'vue'
+import { ref} from 'vue'
 import { useMovieStore } from '@/store/movies'
-import {searchMovies} from "@/api/movies";
 
 const searchInput = ref('')
 const MovieStore = useMovieStore()
-
-const currentPage = computed(() => {
-  return MovieStore.getCurrentPage;
-})
-
-const searchTerm = computed(() => {
-  return MovieStore.getSearchTerm;
-})
-
-const fetchMovies = async () => {
-  const response = await searchMovies(searchTerm.value || '', currentPage.value)
-  MovieStore.updateMovies(response)
-}
 
 let timeout: ReturnType<typeof setTimeout> | null = null
 
@@ -39,7 +31,14 @@ const handleInput = () => {
     MovieStore.setSearchTerm(searchInput.value)
     MovieStore.setCurrentPage(1)
     MovieStore.fetchMovies()
-  }, 400) // debounce
+  }, 400)
+}
+
+const clearSearchHandler = () => {
+  searchInput.value = ''
+  MovieStore.setSearchTerm('')
+  MovieStore.setCurrentPage(1)
+  MovieStore.fetchMovies()
 }
 </script>
 
